8b18d96d94481cec003cdb79d0437e44e89224c8
gmcalabr <175723646+gmcalabr@users.noreply.github.com>
2024-11-11 13:48:23 -0500

    This is the final edit to Rock Paper Scissors
    All edits have been made and tested. The messages are all neat,
    capitalized. The verb messages (scissors cuts paper, etc) are complete
    and working.

--- script.js
+++ script.js
@@ -38,38 +38,30 @@ while (notDoneYet) {
         }
 }
 
-// Game Ends Here
+// Game Ends Here //
 
-// functions!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// function declarations //
 
-// Main Round Play Function
+// MAIN FUNCTION Main Round Play Function
 function playRound() {
 
     // Collect the human player's choice
     let humanChoice = "0";
     humanChoice = getHumanChoice();
-    console.log("The human chose: ", humanChoice);
 
     // Calculate the computer player's choice
     let computerChoice = "0";
     computerChoice = getComputerChoice();
-    console.log("The computer chose: ", computerChoice);
 
     // Compare player hands and report
     let roundPoint = determineRoundWinner(humanChoice, computerChoice);
 
-    // Generate a message to tell player that X beats Y hand
-    roundCompareMessage(humanChoice, computerChoice, roundPoint);
-
     // Change the scoreboard and report
     scoreChange(roundPoint);
-    console.log("~~~~~Current Score");
-    console.log("Human: ", humanPoints);
-    console.log("Computer: ", computerPoints);
-    return
+    return ;
 }
 
-// Poll human for RPS choice
+// L2 FUNCTION Poll human for RPS choice
 function getHumanChoice() {
     // Declare local variables
     let hand = "0";
@@ -84,14 +76,16 @@ function getHumanChoice() {
         //check input
         if (hand === "rock" || hand === "paper" || hand === "scissors") {
             keepGoing = false;
-            return hand;
         } else {
             hand = alert("The human has entered their choice incorrectly. The human must enter either Rock, Paper, or Scissors");
         }
     }
+    // Deliver the user their choice
+    console.log("The human chose: ", hand);
+    return hand;
 }
 
-// Generate computer's RPS choice
+// L2 FUNCTION Generate computer's RPS choice
 function getComputerChoice() {
     
     // Declare variables
@@ -112,72 +106,83 @@ function getComputerChoice() {
         console.log("ERROR, cpuChoiceNum is out of range");
         console.log("cpuChoiceNum = ${cpuChoiceNum}");
     }
-
+    // Let user know what the computer picked
+    console.log("The computer chose: ", hand);
     return hand;
 }
 
-// Select Round winner
+// L2 FUNCTION Select Round winner
 function determineRoundWinner(humanChoice, computerChoice) {
+    // Declare message style variable
+    let compareVerb = "0";
+
     // Calculate who won and assign 0 (tie), 1 (human win), or -1 (computer win)
     if (humanChoice == computerChoice) {
+        compareVerb = " ties ";
+        console.log(capitalizeFirstLetter(humanChoice + compareVerb + computerChoice));
         console.log("Tie round")
         return 0;
-    } else if ((humanChoice == "rock" && computerChoice == "scissors") || 
-               (humanChoice == "paper" && computerChoice == "rock") || 
-               (humanChoice == "scissors" && computerChoice == "paper")) {
+    } else if ((humanChoice == "rock" && computerChoice == "scissors") || (humanChoice == "paper" && computerChoice == "rock") || (humanChoice == "scissors" && computerChoice == "paper")) {
+        compareVerb = compareVerbDecider(humanChoice);
+        console.log(capitalizeFirstLetter(humanChoice + compareVerb + computerChoice));
         console.log("Human wins")
         return 1;
     } else {
+        compareVerb = compareVerbDecider(computerChoice);
+        console.log(capitalizeFirstLetter(computerChoice + compareVerb + humanChoice));
         console.log("Computer wins")
         return -1;
     }
 }
+// L3 Function decide what verb compares the losing and wining hands
+function compareVerbDecider(winnerChoice) {
 
-// Generate a nice "X beats Y" message
-function roundCompareMessage(humanChoice, computerChoice, roundPoint) {
+    // define compareVerb for use
+    let compareVerb = "0";
 
-    // Write the message based on hand
-    if (roundPoint = 1 && humanChoice == "rock") {
-        console.log("Rock crushes Scissors");
-    } else if (roundPoint = -1 && computerChoice == "rock") {
-        console.log("Rock crushes Scissors");
-    } else if (roundPoint = 1 && humanChoice == "paper") {
-        console.log("Paper covers Rock");
-    } else if (roundPoint = -1 && computerChoice == "paper") {
-        console.log("Paper covers Rock");
-    } else if (roundPoint = 1 && humanChoice == "scissors") {
-        console.log("Scissors cuts Paper");
-    } else if (roundPoint = -1 && computerChoice == "scissors") {
-        console.log("Scissors cuts Paper");
-    } else {
-        console.log(humanChoice, " ties ", computerChoice);
+    // calulate the compare verb based on how the winner won 
+    if (winnerChoice == "rock" ) {
+        compareVerb = " crushes ";
+    } else if (winnerChoice == "paper") {
+        compareVerb = " covers ";
+    } else if (winnerChoice == "scissors") {
+        compareVerb = " cuts ";
     }
+    
+    return compareVerb;
 }
 
-// Score adder
+
+// L4 Function Captialize the first letter function
+function capitalizeFirstLetter(statement) {
+    return String(statement).charAt(0).toUpperCase() + String(statement).slice(1);
+}
+
+// L4 Function Score adder and messanger
 function scoreChange(roundPoint) {
     roundNumber = roundNumber + 1;
     //if roundPoints is 0, output existing score
-    if (roundPoint == 0) {
-        return
-    } else if (roundPoint > 0) {
+    if (roundPoint > 0) {
         humanPoints = humanPoints + 1;
-        return
-    } else {
+    } else if (roundPoint < 0) {
         computerPoints = computerPoints + 1;
-        return
     }
+
+    console.log("~~~~~Current Score");
+    console.log("Human: ", humanPoints);
+    console.log("Computer: ", computerPoints);
+    return ;
 }
 
-// Calculate/display End of Game Score
+// MAIN FUNCTION Calculate/display End of Game Score
 function gameFinalScore() {
 
     // Declare game finals
     console.log("--------------------------------------------");
-    console.log("~~~~~The game is over. ~~~~~ The score is ~~~~~");
+    console.log("~~~~~The game is over. ~~~~~ The final score is ~~~~~");
 
-    console.log("Computer: ", computerPoints);
     console.log("Human: ", humanPoints);
+    console.log("Computer: ", computerPoints);
 
     // If statement to output correct endgame description
     if (computerPoints === humanPoints) {
@@ -193,7 +198,7 @@ function gameFinalScore() {
     }
 }
 
-// Reet Game
+// MAIN FUNCTION Reet Game
 function resetGame() {
     computerPoints = 0;
     humanPoints = 0;
